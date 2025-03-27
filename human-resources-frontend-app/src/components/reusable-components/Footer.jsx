87ef5f6d2dc83cc33ecaaeeb4136eb48677957ae
Anastasija Milicev <milicevana3@gmail.com>
2025-03-27 22:25:29 +0100

    Dodata reusable komponenta za vreme u footer

--- human-resources-frontend-app/src/components/reusable-components/Footer.jsx
+++ human-resources-frontend-app/src/components/reusable-components/Footer.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import Weather from '../reusable-components/Weather';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>
-        Having problems? Contact our support: <> </>
-        <a href="mailto:hrDashboard@support.rs" className="footer-email">
-           hrDashboard@support.rs
-        </a>
-      </p>
+      <div className="footer-left">
+        {/* Display the small weather widget for Belgrade */}
+        <Weather latitude={44.7866} longitude={20.4489} />
+      </div>
+      <div className="footer-right">
+        <p>
+          Having problems? Contact our support:&nbsp;
+          <a href="mailto:hrDashboard@support.rs" className="footer-email">
+            hrDashboard@support.rs
+          </a>
+        </p>
+      </div>
     </footer>
   );
 };
